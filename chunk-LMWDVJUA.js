2083849ac9a7000734370c2321bd36e5f9c4a7a7
Salah Ebrahim <salah.saber@flairstech.com>
2025-01-05 18:58:14 +0200

    Auto-generated commit

---
+++ chunk-LMWDVJUA.js
@@ -0,0 +1,1 @@
+import{s as a}from"./chunk-ZIPZ42J7.js";import"./chunk-VRF6LYDG.js";export{a as MoviesModule};
---
+++ chunk-QPYCHDWH.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-LI6EGYF3.js";import"./chunk-VRF6LYDG.js";export{a as DashboardModule};
